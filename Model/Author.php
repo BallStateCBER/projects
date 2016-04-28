3e0921e0b5e0c8ab3422184600ab5b7e87fefba2
Graham Watson <graham@phantomwatson.com>
2016-04-27 23:00:07 -0400

    Fix deprecated Validation::notEmpty()

--- Model/Author.php
+++ Model/Author.php
@@ -22,7 +22,7 @@ class Author extends AppModel {
 	public $validate = array(
 		'name' => array(
 			'notEmpty' => array(
-				'rule' => array('notEmpty'),
+				'rule' => array('notBlank'),
 				//'message' => 'Your custom message here',
 				//'allowEmpty' => false,
 				//'required' => false,
